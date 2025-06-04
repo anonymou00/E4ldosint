d1846e493a1db50280c6d83d4ca61b0fe0d089af
e4lord <162916990+anonymou00@users.noreply.github.com>
2025-06-04 21:20:53 +0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+import subprocess
+
+subprocess.run(["sudo", "apt", "install", "golang", "-y"])
+subprocess.run(["sudo", "apt", "install", "-y", "whois"])
+subprocess.run(["sudo", "apt", "install", "-y", "nikto"])
+subprocess.run(["sudo", "apt", "install", "-y", "whatweb"])
+subprocess.run(["sudo", "apt", "install", "-y", "dnsenum"])
+subprocess.run(["sudo", "apt", "install", "-y", "assetfinder"])
+subprocess.run(["sudo", "apt", "install", "-y", "wafw00f"])
+subprocess.run(["sudo", "apt", "install", "-y", "theharvester"])
+subprocess.run(["go", "install", "-v", "github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest"])
+subprocess.run(["sudo", "apt", "install", "-y", "dirsearch"])
